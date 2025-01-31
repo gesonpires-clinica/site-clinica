f0ac6c4254746f3ef272825e08488a5e3bc95ec2
gesonpires-clinica <clinicaneuromarianebach@gmail.com>
2025-01-31 14:09:34 -0300

    Exclusão do número do agendamento.

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
-  appointmentId: { type: Number, unique: true }, // Número sequencial único
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
